1273789c1a5bbbacd2d7c6b157ac866850781471
Ferry pratama <ftama2402@gmail.com>
2022-03-11 23:26:18 +0800

    backup

---
+++ js/main.js
@@ -0,0 +1,80 @@
+(function($) {
+  "use strict";
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  var scrolltoOffset = $('#main-nav').outerHeight() - 1;
+  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        e.preventDefault();
+
+        var scrollto = target.offset().top - scrolltoOffset;
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if (window.matchMedia("(max-width:991px)").matches) {
+          $('.nav-menu').hide();
+        }
+        return false;
+      }
+    }
+  });
+
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  // ========================================================================= //
+  //  //NAVBAR SHOW - HIDE
+  // ========================================================================= //
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (!$('.subpage-nav').length) {
+      if (scroll > 200) {
+        $("#main-nav").slideDown(700);
+      } else {
+        $("#main-nav").slideUp(700);
+      }
+    }
+  });
+
+  // ========================================================================= //
+  //  // RESPONSIVE MENU
+  // ========================================================================= //
+
+  $('.responsive').on('click', function(e) {
+    $('.nav-menu').slideToggle();
+  });
+
+  // ========================================================================= //
+  //  Typed Js
+  // ========================================================================= //
+
+  var typed = $(".typed");
+
+  $(function() {
+    var strings = $('.typed-items').text();
+    strings = $('.typed-items').data('typed-person') + ',' + strings;
+    strings = strings.split(',');
+
+    typed.typed({
+      strings: strings,
+      typeSpeed: 100,
+      loop: true,
+    });
+  });
+
+})(jQuery);
